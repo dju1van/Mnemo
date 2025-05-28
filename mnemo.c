f582add1954060c4468aac1429174687b772cfae
dju1van <ivan.djuric15@gmail.com>
2025-05-28 21:44:17 +0200

    Dodana simulacija prompta sa najosnovnijim funkcijama

--- mnemo.c
+++ mnemo.c
@@ -9,16 +9,51 @@
  * MIC/MAC za provjeru integriteta.
  */
 
-#define FILENAME "justYourEverydayFile.bin"
-
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdlib.h>
+#include <openssl/evp.h>
+
+#define FILENAME "just_your_everyday_file.bin"
+#define MAX_INPUT 256
+
+// Custom funkcija za makivanje whitespaceova u inputu korisnika
+char* trim(char* string) {
+    char* end;
+
+    // Makni whitespaceove ispred
+    while(isspace((unsigned char)*string)) string++;
+
+    // Ako je bila prazna linija vrati odmah
+    if (*string == 0) 
+        return string;
+
+    // Uzmi zadnji znak inputa
+    end = string + strlen(string) - 1;
+    // Makni whitespaceove odozada
+    while (end > string && isspace((unsigned char)*end)) end--;
+
+    // Null terminiraj input
+    *(end + 1) = '\0';
+
+    return string;
+}
+
+// Funkcija za inicijalizaciju binarne datoteke
+void init() {
+
+}
+
+// Funkcija za dekripciju datoteke
+void decrypt() {
+
+}
 
 int main(int argc, char* argv[]) {
     // Provjera ulaza
     if(argc != 2) {
-        printf("Usage: mnemo <master_password>");
+        printf("Usage: mnemo [master_password]");
         return 1;
     }
 
@@ -44,6 +79,35 @@ int main(int argc, char* argv[]) {
         } else {
             printf("Input error.");
             return 1;
+        }
+    }
+
+    // Buffer za unos korisnika u simuliranom shellu
+    char input[MAX_INPUT];
+
+    // Beskonacna petlja za simuliranje shella
+    while(1) {
+        // Ispis
+        printf("tool > ");
+        fflush(stdout);
+
+        // Citaj ulaz korisnika
+        if(fgets(input, MAX_INPUT, stdin) == NULL) break;
+
+        // Makni LF iz korisnikovog inputa
+        input[strcspn(input, "\n")] = 0;
+        // Trimaj input korisnika
+        char *input_t = trim(input);
+
+        // Naredba 'exit' izlazi iz simuliranog shella
+        if(strcmp(input_t, "exit") == 0) {
+            break;
+        } else if(strlen(input_t) == 0) {   // Ignoriraj prazne linije
+            continue;
+        } else if(strcmp(input_t, "clear") == 0) {  // Simulira ciscenje prompta
+            system("cls");
+        } else {
+            printf("Unknown command.\n");
         }
     }
 }
